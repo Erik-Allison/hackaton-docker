1d35a8e00bf0ca1304f53e1d04f7d41b8067f691
Allison H <eherazo@lsv-tech.com>
2022-06-08 10:40:37 -0500

    creacion de pryecto con docker y modelos de hospital

---
+++ hospitalApp/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+
+from hospitalApp.models import (Especializades, Medicos, Pacientes, Horarios,
+                                Citas)
+
+@admin.register(Especializades)
+class EspecializadesAdmin(admin.ModelAdmin):
+    list_display=(
+        'id',
+        'Nombre',
+        'FechaRegistro',
+    )
+    
+
+@admin.register(Medicos)
+class Medicos(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'Nombres',
+        'Apellidos',
+        'Dni',
+        'EspecialidadId'
+    )
+    
+@admin.register(Pacientes)
+class PacientesAdmin(admin.ModelAdmin):
+    list_display = (
+        'Nombres',
+        'Apellidos',
+        'Telefono',
+        'Activo',
+    )
+    
+@admin.register(Horarios)
+class HorariosAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'FechaAtencion',
+        'InicioAtencio',
+        'FechaRegistro',
+        'FechaModificacion',
+    )
+    
+@admin.register(Citas)
+class CitasAdmin(admin.ModelAdmin):
+    list_display = (
+        'FechaAtencion',
+        'InicioAtencion',
+        'FinAtencion',
+        'Activo',
+        'FechaModificacion',
+    )
---
+++ hospitalApp/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+# Create your models here.
+class Especializades(models.Model):
+    Nombre = models.CharField(max_length=50)
+    Descripcion = models.CharField(max_length=150)
+    FechaRegistro = models.DateField(auto_now_add=True)
+    FechaModificacion = models.DateField(auto_now_add=True)
+    UsuarioRegistro = models.CharField(max_length=30)
+    UsuarioModificacion = models.CharField(max_length=30)
+    Activo = models.BooleanField(default=True)
+    
+class Medicos(models.Model):
+    Nombres = models.CharField(max_length=50)
+    Apellidos = models.CharField(max_length=50)
+    Dni = models.CharField(max_length=30)
+    EspecialidadId = models.ForeignKey(Especializades, on_delete=models.CASCADE) 
+    Direccion = models.CharField(max_length=150)
+    correo = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=20)
+    sexo = models.CharField(max_length=20)
+    NumColegiatura = models.CharField(max_length=30)
+    FechaNacimiento = models.DateField(auto_now_add=False)
+    FechaRegistro = models.DateField(auto_now_add=True)
+    FechaModificacion = models.DateField(auto_now_add=True)
+    UsuarioRegistro = models.CharField(max_length=30)
+    Activo = models.BooleanField(default=True)
+
+class Pacientes(models.Model):
+    Nombres = models.CharField(max_length=50)
+    Apellidos = models.CharField(max_length=50)
+    Dni = models.CharField(max_length=30)
+    Direccion = models.CharField(max_length=100)
+    Telefono = models.CharField(max_length=20)
+    sexo = models.CharField(max_length=20)
+    FechaNacimiento = models.DateField(auto_now_add=False)
+    FechaResgistro = models.DateField(auto_now_add=True)
+    FechaModificacion = models.DateField(auto_now_add=True)
+    UsuarioRegistro = models.CharField(max_length=50)
+    UsuarioModificacion = models.CharField(max_length=50)
+    Activo = models.BooleanField(default=True)
+    
+class Horarios(models.Model):
+    MedicoId = models.ManyToManyField(Medicos)
+    FechaAtencion = models.DateField(auto_now_add=False)
+    InicioAtencio = models.DateTimeField(auto_now_add=False)
+    FinAtencion = models.DateTimeField(auto_now_add=False)
+    Activo = models.BooleanField(default=True)
+    FechaRegistro = models.DateField(auto_now_add=True)
+    UsuarioRegistro = models.CharField(max_length=50)
+    FechaModificacion = models.DateField(auto_now_add=True)
+    UsuarioModificacion = models.CharField(max_length=50)
+    
+class Citas(models.Model):
+    MedicoId = models.ForeignKey(Medicos, on_delete=models.CASCADE)
+    PacienteId = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    FechaAtencion = models.DateField(auto_now_add=False)
+    InicioAtencion = models.DateTimeField(auto_now_add=False)
+    FinAtencion = models.DateTimeField(auto_now_add=False)
+    Estado = models.BooleanField(default=True)
+    Observaciones = models.TextField()
+    Activo = models.BooleanField(default=True)
+    FechaRegistro = models.CharField(max_length=50)
+    UsuarioRegistro = models.CharField(max_length=50)
+    FechaModificacion = models.DateField(auto_now_add=True)
+    UsuarioModificacion = models.CharField(max_length=50)
