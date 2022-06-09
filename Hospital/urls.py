6d3153b99f4d4c26087019bbcf5831ab2d02fcd1
Allison H <eherazo@lsv-tech.com>
2022-06-09 14:44:52 -0500

    modelos serializados por completo

--- Hospital/urls.py
+++ Hospital/urls.py
@@ -1,12 +1,22 @@
 from django.urls import include, path
-from hospitalApp.views import EspecialidadesViewSet
+
+from hospitalApp.views import (CitasViewSet, EspecialidadesViewSet, HorariosViewSet, MedicosViewSet,
+                               PacientesViewSet)
+
 from rest_framework import routers
-from hospitalApp import views
 
 router = routers.DefaultRouter()
 router.register(r'Especialidades', EspecialidadesViewSet)
+router.register(r'Medicos', MedicosViewSet)
+router.register(r'Pacientes', PacientesViewSet)
+router.register(r'Horarios', HorariosViewSet)
+router.register(r'Citas', CitasViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('especialidades/', include('rest_framework.urls', namespace='rest_framework'))
+    path('especialidades/', include('rest_framework.urls', namespace='rest_framework')),
+    path('medicos/', include('rest_framework.urls', namespace='rest_framework')),
+    path('pacientes/', include('rest_framework.urls', namespace='rest_framework')),
+    path('horarios/', include('rest_framework.urls', namespace='rest_framework')),
+    path('citas/', include('rest_framework.urls', namespace='rest_framework')),
 ]
