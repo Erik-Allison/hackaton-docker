d76b8fac69f6b1dec575706986e6c7c772ca3506
Allison H <eherazo@lsv-tech.com>
2022-06-09 15:11:29 -0500

    modelos serializados por completo

--- hospitalApp/serializers.py
+++ hospitalApp/serializers.py
@@ -9,3 +9,35 @@ class EspecialidadesSerializer(serializers.HyperlinkedModelSerializer):
         model = Especialidades
         fields = ['Nombre', 'Descripcion', 'FechaRegistro', 'FechaModificacion', 'UsuarioRegistro',
                   'UsuarioModificacion', 'Activo']
+
+
+class MedicosSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Medicos
+        fields = ['Nombres', 'Apellidos', 'Dni', 'EspecialidadId', 'Direccion',
+                  'correo', 'telefono', 'sexo', 'NumColegiatura', 'FechaNacimiento', 'FechaRegistro',
+                  'FechaModificacion', 'UsuarioRegistro', 'Activo']
+
+
+class PacientesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Pacientes
+        fields = ['Nombres', 'Apellidos', 'Dni', 'Direccion',
+                  'Telefono', 'sexo', 'FechaNacimiento', 'FechaResgistro', 'FechaModificacion',
+                  'UsuarioRegistro', 'UsuarioModificacion', 'Activo']
+
+
+class HorariosSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Horarios
+        fields = ['MedicoId', 'FechaAtencion', 'InicioAtencio', 'FinAtencion',
+                  'Activo', 'FechaRegistro', 'UsuarioRegistro', 'FechaModificacion',
+                  'UsuarioModificacion']
+
+
+class CitasSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Citas
+        fields = ['MedicoId', 'PacienteId', 'FechaAtencion', 'InicioAtencion',
+                  'FinAtencion', 'Estado', 'Observaciones', 'Activo', 'FechaRegistro',
+                  'UsuarioRegistro', 'FechaModificacion', 'UsuarioModificacion']
--- hospitalApp/views.py
+++ hospitalApp/views.py
@@ -1,7 +1,5 @@
-#from django.shortcuts import render
-
-# Create your views here.
-from hospitalApp.serializers import EspecialidadesSerializer
+from hospitalApp.serializers import (CitasSerializer, EspecialidadesSerializer,
+                                     HorariosSerializer, MedicosSerializer, PacientesSerializer)
 from rest_framework import viewsets
 from .models import (Especialidades, Medicos, Pacientes, Horarios, Citas)
 
@@ -9,3 +7,23 @@ from .models import (Especialidades, Medicos, Pacientes, Horarios, Citas)
 class EspecialidadesViewSet(viewsets.ModelViewSet):
     queryset = Especialidades.objects.all()
     serializer_class = EspecialidadesSerializer
+
+
+class MedicosViewSet(viewsets.ModelViewSet):
+    queryset = Medicos.objects.all()
+    serializer_class = MedicosSerializer
+
+
+class PacientesViewSet(viewsets.ModelViewSet):
+    queryset = Pacientes.objects.all()
+    serializer_class = PacientesSerializer
+
+
+class HorariosViewSet(viewsets.ModelViewSet):
+    queryset = Horarios.objects.all()
+    serializer_class = HorariosSerializer
+
+
+class CitasViewSet(viewsets.ModelViewSet):
+    queryset = Citas.objects.all()
+    serializer_class = CitasSerializer
